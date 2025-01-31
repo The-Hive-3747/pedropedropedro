9d05f05d85d70fa90ccf10d3f6c1c19bb8eaed3a
The-Hive-3747 <3747hive@gmail.com>
2025-01-31 16:40:24 -0700

    ????

--- TeamCode/src/main/java/pedroPathing/examples/fiveSpeciAHHHH.java
+++ TeamCode/src/main/java/pedroPathing/examples/fiveSpeciAHHHH.java
@@ -16,202 +16,60 @@ import pedroPathing.constants.FConstants;
 import pedroPathing.constants.LConstants;
 
 @Autonomous(name = "five speci ahh", group = "Examples")
-    public class fiveSpeciAHHHH extends OpMode {
-    private final Pose startPose = new Pose(8,61.5, Math.toRadians(0));  // Starting position
+public class fiveSpeciAHHHH extends OpMode {
+    
+    public static Pose startPose = new Pose(8,61.5, Math.toRadians(0));
+
+    
     private Follower follower;
     private Timer pathTimer, actionTimer, opmodeTimer;
-    private Path one;
     private int pathState;
-    private PathChain pushSamples, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen;
+
+    public static PathChain preload() {
+        return new PathBuilder()
+            .addPath(newBezierLine(
+                new Point(8, 61.5, Point.CARTESIAN),
+                new Point(41, 75, Point.CARTESIAN)))
+            .setLinearHeadingInterpolation(Math.toRadians(0),Math.toRadians(0))
+            .build();
+
+    public static PathChain pushSamples() {
+        return new PathBuilder()
+            .addPath(new BezierCurve( // after place, goes to first sample
+                new Point(41, 75, Point.CARTESIAN),
+                new Point(0.7, 43.8, Point.CARTESIAN),
+                new Point(65, 28, Point.CARTESIAN)))
+            .setLinearHeadingInterpolation(Math.toRadians(0),Math.toRadians(0))
+            .addPath(new BezierCurve( // pushes first sample
+                new Point(65, 28, Point.CARTESIAN),
+                new Point(65, 22, Point.CARTESIAN),
+                new Point(15, 25, Point.CARTESIAN)))
+            .setLinearHeadingInterpolation(Math.toRadians(0),Math.toRadians(0))
+            .addPath(new BezierLine( // goes to second sample
+                new Point(15, 25, Point.CARTESIAN),
+                new Point(65, 23, Point.CARTESIAN)))
+            .setLinearHeadingInterpolation(Math.toRadians(0),Math.toRadians(0))
+            .addPath(new BezierCurve( // pushes second sample
+                new Point(65, 23, Point.CARTESIAN),
+                new Point(65, 10, Point.CARTESIAN),
+                new Point(15, 15, Point.CARTESIAN)))
+            .setLinearHeadingInterpolation(Math.toRadians(0),Math.toRadians(0))
+            .addPath(new BezierLine( // goes to third sample
+                new Point(15, 15, Point.CARTESIAN),
+                new Point(65, 13, Point.CARTESIAN)))
+            .setLinearHeadingInterpolation(Math.toRadians(0),Math.toRadians(0))
+            .addPath(new BezierCurve( // pushes third sample
+                new Point(65, 13, Point.CARTESIAN),
+                new Point(65, 2, Point.CARTESIAN),
+                new Point(15, 3, Point.CARTESIAN)))
+            .setLinearHeadingInterpolation(Math.toRadians(0),Math.toRadians(0))
+            .build();
+                
+            
+            
+            .
 public void buildPaths() {
-    // Path for scoring preload
-    one = new Path(new BezierLine(
-            new Point(8,61.5,Point.CARTESIAN),
-            new Point(41,75,Point.CARTESIAN)
-    ));
-    one.setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0));
-  
-    pushSamples = follower.pathBuilder()
-            .addPath(new BezierCurve(
-                    new Point(41,75,Point.CARTESIAN),
-                    new Point(0.7,43.8,Point.CARTESIAN),
-                    new Point(65,28,Point.CARTESIAN)
-            ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .addPath(new BezierCurve( //three
-                    new Point(65, 28, Point.CARTESIAN),
-                    new Point(65, 22, Point.CARTESIAN),
-                    new Point(15, 25, Point.CARTESIAN)
-            ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .addPath(new BezierLine( // four
-                    new Point(15, 25, Point.CARTESIAN),
-                    new Point(65, 23, Point.CARTESIAN)
-            ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .addPath(new BezierCurve( // five
-                    new Point(65, 23, Point.CARTESIAN),
-                    new Point(65,10, Point.CARTESIAN),
-                    new Point(15,15,Point.CARTESIAN)
-            ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .addPath(new BezierLine( // six
-                    new Point(15, 15, Point.CARTESIAN),
-                    new Point(65, 13, Point.CARTESIAN)
-            ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .addPath(new BezierCurve( // seven
-                    new Point(65, 13, Point.CARTESIAN),
-                    new Point(65, 2, Point.CARTESIAN),
-                    new Point(15, 3, Point.CARTESIAN)
-            ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .build();
-    /*
-    four = follower.pathBuilder()
-            .addPath(new BezierLine(
-                    new Point(15, 30, Point.CARTESIAN),
-                    new Point(65, 28, Point.CARTESIAN)
-            ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .addPath(new BezierCurve(
-                    new Point(65, 28, Point.CARTESIAN),
-                    new Point(65,20, Point.CARTESIAN),
-                    new Point(15,20,Point.CARTESIAN)
-            ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .setPathEndTimeoutConstraint(3.0)
-            .build();
-
-    five = follower.pathBuilder()
-            .addPath(new BezierCurve(
-          new Point(15, 30, Point.CARTESIAN),
-          new Point(70, 10, Point.CARTESIAN),
-          new Point(50, 10, Point.CARTESIAN)
-        ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .build();
-
-    six = follower.pathBuilder()
-            .addPath(new BezierLine(
-          new Point(50, 10, Point.CARTESIAN),
-          new Point(15, 10, Point.CARTESIAN)
-        ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .build();
-
-    seven = follower.pathBuilder()
-            .addPath(new BezierLine(
-          new Point(15, 10, Point.CARTESIAN),
-          new Point(30, 30, Point.CARTESIAN)
-        ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .build();
-
-    eight = follower.pathBuilder()
-            .addPath(new BezierLine(
-          new Point(30, 30, Point.CARTESIAN),
-          new Point(10, 30, Point.CARTESIAN)
-        ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .build();
-
-    nine = follower.pathBuilder()
-            .addPath(new BezierCurve(
-          new Point(10, 30, Point.CARTESIAN),
-          new Point(10, 71, Point.CARTESIAN),
-          new Point(28, 75, Point.CARTESIAN)
-        ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .build();
-
-    ten = follower.pathBuilder()
-            .addPath(new BezierCurve(
-          new Point(48.768, 74.837, Point.CARTESIAN),
-          new Point(16.670, 71.291, Point.CARTESIAN),
-          new Point(18.266, 31.567, Point.CARTESIAN)
-        ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .build();
-
-    eleven = follower.pathBuilder()
-            .addPath(new BezierLine(
-          new Point(18.266, 31.567, Point.CARTESIAN),
-          new Point(3.369, 31.921, Point.CARTESIAN)
-        ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .build();
-
-    twelve = follower.pathBuilder()
-            .addPath(new BezierCurve(
-          new Point(3.369, 31.921, Point.CARTESIAN),
-          new Point(17.025, 70.936, Point.CARTESIAN),
-          new Point(48.946, 71.291, Point.CARTESIAN)
-        ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .build();
-
-    thirteen = follower.pathBuilder()
-            .addPath(new BezierCurve(
-          new Point(48.946, 71.291, Point.CARTESIAN),
-          new Point(17.202, 71.113, Point.CARTESIAN),
-          new Point(17.557, 31.744, Point.CARTESIAN)
-        ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .build();
-
-    fourteen = follower.pathBuilder()
-            .addPath(new BezierLine(
-          new Point(17.557, 31.744, Point.CARTESIAN),
-          new Point(3.015, 31.389, Point.CARTESIAN)
-        ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .build();
-
-    fifteen = follower.pathBuilder()
-            .addPath(new BezierCurve(
-          new Point(3.015, 31.389, Point.CARTESIAN),
-          new Point(7.803, 62.069, Point.CARTESIAN),
-          new Point(48.414, 67.567, Point.CARTESIAN)
-        ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .build();
-
-    sixteen = follower.pathBuilder()
-            .addPath(new BezierCurve(
-          new Point(48.414, 67.567, Point.CARTESIAN),
-          new Point(15.429, 65.261, Point.CARTESIAN),
-          new Point(17.557, 31.212, Point.CARTESIAN)
-        ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .build();
-
-    seventeen = follower.pathBuilder()
-            .addPath(new BezierLine(
-          new Point(17.557, 31.212, Point.CARTESIAN),
-          new Point(3.015, 31.212, Point.CARTESIAN)
-        ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .build();
-
-    eighteen = follower.pathBuilder()
-            .addPath(new BezierCurve(
-          new Point(3.015, 31.212, Point.CARTESIAN),
-          new Point(10.463, 62.069, Point.CARTESIAN),
-          new Point(48.059, 63.665, Point.CARTESIAN)
-        ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .build();
-
-    nineteen = follower.pathBuilder()
-            .addPath(new BezierCurve(
-          new Point(48.059, 63.665, Point.CARTESIAN),
-          new Point(11.350, 64.552, Point.CARTESIAN),
-          new Point(8.512, 29.261, Point.CARTESIAN)
-        ))
-            .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
-            .build();
-    */
+        
 }
 
 public void autonomousPathUpdate() {
